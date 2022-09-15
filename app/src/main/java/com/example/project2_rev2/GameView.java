e9c1457ac577530d753de3afaec1a3dec4c63e6f
Guy Oz <109523947+GuyOz5252@users.noreply.github.com>
2022-09-15 16:31:16 +0300

    added register and started drag n drop work

--- app/src/main/java/com/example/project2_rev2/GameView.java
+++ app/src/main/java/com/example/project2_rev2/GameView.java
@@ -2,13 +2,40 @@ package com.example.project2_rev2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
+import android.content.ClipDescription;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class GameView extends AppCompatActivity {
+
+    TextView txt1, target;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_view);
+
+        txt1 = findViewById(R.id.text1);
+        target = findViewById(R.id.target);
+
+        txt1.setOnLongClickListener(longClickListener);
+        target.setOnLongClickListener(longClickListener);
     }
+
+    View.OnLongClickListener longClickListener = new View.OnLongClickListener() {
+        @Override
+        public boolean onLongClick(View view) {
+
+            ClipData.Item item = new ClipData.Item((CharSequence) view.getTag());
+            String[] mimeTypes = {ClipDescription.MIMETYPE_TEXT_PLAIN};
+            ClipData data = new ClipData(view.getTag().toString(), mimeTypes, item);
+            View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
+
+            view.startDragAndDrop(data, shadowBuilder, view, 0);
+            return true;
+        }
+    };
+
 }
--- app/src/main/java/com/example/project2_rev2/LogIn.java
+++ app/src/main/java/com/example/project2_rev2/LogIn.java
@@ -3,6 +3,7 @@ package com.example.project2_rev2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.MotionEvent;
@@ -14,7 +15,7 @@ import android.widget.TextView;
 public class LogIn extends AppCompatActivity implements View.OnTouchListener {
 
     // menu elements
-    Button btnLogin, btnRegister;
+    Button btnLogin, btnRegister, btnDebugGame;
 
     // login dialog elements
     Dialog login;
@@ -22,14 +23,32 @@ public class LogIn extends AppCompatActivity implements View.OnTouchListener {
     EditText edtEmailLoginD, edtPasswordLoginD;
     TextView txtRegisterLoginD;
 
+    // register dialog elements
+    Dialog register;
+    Button btnRegisterRegisterD;
+    EditText edtEmailRegisterD, edtPasswordRegisterD, edtConPasswordRegisterD;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_log_in);
+        setContentView(R.layout.activity_login);
 
         btnLogin = findViewById(R.id.btnLogin);
+        btnRegister = findViewById(R.id.btnRegister);
+        btnDebugGame = findViewById(R.id.btnDebugGame);
 
         btnLogin.setOnTouchListener(this);
+        btnRegister.setOnTouchListener(this);
+        btnDebugGame.setOnTouchListener(this);
+    }
+
+    public void debugGame(View view, MotionEvent motionEvent) {
+        if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+            startActivity(new Intent(this, GameView.class));
+            view.setAlpha(1);
+        } else if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+            view.setAlpha((float)0.5);
+        }
     }
 
     //============login dialog=============//
@@ -115,7 +134,7 @@ public class LogIn extends AppCompatActivity implements View.OnTouchListener {
 
     public void clickedRegisterInDialog() {
         login.dismiss();
-        //createRegisterDialog();
+        createRegisterDialog();
     }
 
     public void clickedRegisterInDialog(View view, MotionEvent motionEvent) {
@@ -126,14 +145,92 @@ public class LogIn extends AppCompatActivity implements View.OnTouchListener {
             view.setAlpha((float)0.5);
         }
     }
-
     //=====================================//
+
+    //==========register dialog============//
+    public void createRegisterDialog() {
+        register = new Dialog(this);
+        register.setContentView(R.layout.dialog_register);
+        register.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION); // hide bottom bar
+        register.getWindow().setBackgroundDrawableResource(R.drawable.dialog_custom);
+        register.setTitle("Register");
+
+        btnRegisterRegisterD = register.findViewById(R.id.btnRegister_registerDialog);
+        edtEmailRegisterD = register.findViewById(R.id.edtMail_registerDialog);
+        edtPasswordRegisterD = register.findViewById(R.id.edtPassword_registerDialog);
+        edtConPasswordRegisterD = register.findViewById(R.id.edtConPassword_registerDialog);
+
+        btnRegisterRegisterD.setOnTouchListener(this);
+
+        register.show();
+    }
+
+    public void createRegisterDialog(View view, MotionEvent motionEvent) {
+        if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+            createRegisterDialog();
+            view.setAlpha(1);
+        } else if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+            view.setAlpha((float)0.5);
+        }
+    }
+
+    public void clickRegister() {
+        String email = edtEmailRegisterD.getText().toString();
+        String password = edtPasswordRegisterD.getText().toString();
+        String conPassword = edtConPasswordRegisterD.getText().toString();
+
+        if (email.isEmpty()) {
+            edtEmailRegisterD.setError("email required");
+            edtEmailRegisterD.requestFocus();
+            return;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            edtEmailRegisterD.setError("invalid email");
+            edtEmailRegisterD.requestFocus();
+            return;
+        } else if (password.isEmpty()) {
+            edtPasswordRegisterD.setError("password required");
+            edtPasswordRegisterD.requestFocus();
+            return;
+        } else if (password.length() < 6) {
+            edtPasswordRegisterD.setError("password too short, minimum length is 6");
+            edtPasswordRegisterD.requestFocus();
+            return;
+        } else if (conPassword.isEmpty()) {
+            edtConPasswordRegisterD.setError("confirm password");
+            edtConPasswordRegisterD.requestFocus();
+            return;
+        } else if (!conPassword.equals(password)) {
+            edtConPasswordRegisterD.setError("passwords don't match");
+            edtConPasswordRegisterD.requestFocus();
+            return;
+        }
+
+        //register user
+
+        register.dismiss();
+    }
+
+    public void clickRegister(View view, MotionEvent motionEvent) {
+        if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+            clickRegister();
+            view.setAlpha(1);
+        } else if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+            view.setAlpha((float)0.5);
+        }
+    }
+    //====================================//
 
     @Override
     public boolean onTouch(View view, MotionEvent motionEvent) {
         switch (view.getId()) {
             case R.id.btnLogin:
                 createLoginDialog(view, motionEvent);
+                break;
+            case R.id.btnRegister:
+                createRegisterDialog(view, motionEvent);
+                break;
+            case R.id.btnDebugGame:
+                debugGame(view, motionEvent);
                 break;
             //=login dialog=//
             case R.id.btnLogin_loginDialog:
@@ -144,6 +241,10 @@ public class LogIn extends AppCompatActivity implements View.OnTouchListener {
                 break;
             case R.id.txtRegister_loginDialog:
                 clickedRegisterInDialog(view, motionEvent);
+                break;
+            //=register dialog=//
+            case R.id.btnRegister_registerDialog:
+                clickRegister(view, motionEvent);
                 break;
         }
         return true;
