aaf3130470ca27eb7287be38555788e352699e21
Guy Oz <109523947+GuyOz5252@users.noreply.github.com>
2022-09-16 00:05:30 +0300

    setting up for a grid, ready to add

--- app/src/main/java/com/example/project2_rev2/GameView.java
+++ app/src/main/java/com/example/project2_rev2/GameView.java
@@ -12,6 +12,7 @@ import android.view.DragEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,7 +20,7 @@ import android.widget.Toast;
 public class GameView extends AppCompatActivity {
 
     TextView txt1;
-    LinearLayout ln1, ln2;
+    LinearLayout ln;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,54 +28,45 @@ public class GameView extends AppCompatActivity {
         setContentView(R.layout.activity_game_view);
 
         txt1 = findViewById(R.id.text1);
-        ln1 = findViewById(R.id.ln1);
-        ln2 = findViewById(R.id.ln2);
+        ln = findViewById(R.id.ln);
 
         txt1.setOnLongClickListener(longClickListener);
-        ln1.setOnDragListener(dragListener);
-        ln2.setOnDragListener(dragListener);
+        ln.setOnDragListener(dragListener);
     }
 
-    View.OnLongClickListener longClickListener = new View.OnLongClickListener() {
-        @Override
-        public boolean onLongClick(View view) {
-            ClipData.Item item = new ClipData.Item("");
-            String[] mimeTypes = {ClipDescription.MIMETYPE_TEXT_PLAIN};
-            ClipData data = new ClipData("", mimeTypes, item);
-            View.DragShadowBuilder dragShadowBuilder = new View.DragShadowBuilder(view);
-            view.startDragAndDrop(data, dragShadowBuilder, view, 0);
-            view.setVisibility(View.INVISIBLE);
-            return true;
-        }
+    View.OnLongClickListener longClickListener = view -> {
+        ClipData.Item item = new ClipData.Item("");
+        String[] mimeTypes = {ClipDescription.MIMETYPE_TEXT_PLAIN};
+        ClipData data = new ClipData("", mimeTypes, item);
+        View.DragShadowBuilder dragShadowBuilder = new View.DragShadowBuilder(view);
+        view.startDragAndDrop(data, dragShadowBuilder, view, 0);
+        view.setVisibility(View.INVISIBLE);
+        return true;
     };
 
-    View.OnDragListener dragListener = new View.OnDragListener() {
-        @Override
-        public boolean onDrag(View view, DragEvent dragEvent) {
-            switch (dragEvent.getAction()) {
-                case DragEvent.ACTION_DRAG_STARTED:
-                    return dragEvent.getClipDescription().hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN);
-                case DragEvent.ACTION_DRAG_ENTERED:
-                case DragEvent.ACTION_DRAG_EXITED:
-                case DragEvent.ACTION_DRAG_ENDED:
-                    view.invalidate();
-                    return true;
-                case DragEvent.ACTION_DRAG_LOCATION:
-                    return true;
-                case DragEvent.ACTION_DROP:
-                    ClipData.Item item = dragEvent.getClipData().getItemAt(0);
-                    CharSequence dragData = item.getText();
-                    System.out.println(dragData);
-                    view.invalidate();
-                    View v = (View)(dragEvent.getLocalState());
-                    ViewGroup owner = (ViewGroup)(v.getParent());
-                    owner.removeView(v);
-                    ((LinearLayout)view).addView(v);
-                    v.setVisibility(View.VISIBLE);
-                    return true;
-            }
-            return true;
+    View.OnDragListener dragListener = (view, dragEvent) -> {
+        switch (dragEvent.getAction()) {
+            case DragEvent.ACTION_DRAG_STARTED:
+                return dragEvent.getClipDescription().hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN);
+            case DragEvent.ACTION_DRAG_ENTERED:
+            case DragEvent.ACTION_DRAG_EXITED:
+            case DragEvent.ACTION_DRAG_ENDED:
+                view.invalidate();
+                return true;
+            case DragEvent.ACTION_DRAG_LOCATION:
+                return true;
+            case DragEvent.ACTION_DROP:
+                ClipData.Item item = dragEvent.getClipData().getItemAt(0);
+                CharSequence dragData = item.getText();
+                System.out.println(dragData);
+                view.invalidate();
+                View v = (View)(dragEvent.getLocalState());
+                ((ViewGroup)(v.getParent())).removeView(v);
+                ((ViewGroup)view).addView(v);
+                v.setVisibility(View.VISIBLE);
+                return true;
         }
+        return true;
     };
 
 }
