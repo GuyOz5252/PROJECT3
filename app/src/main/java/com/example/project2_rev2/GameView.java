a7e50f4a896a8abb06a899e710a7654d8db26ff7
Guy Oz <109523947+GuyOz5252@users.noreply.github.com>
2022-09-15 21:34:53 +0300

    basic drag n drop

--- app/src/main/java/com/example/project2_rev2/GameView.java
+++ app/src/main/java/com/example/project2_rev2/GameView.java
@@ -4,13 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ClipData;
 import android.content.ClipDescription;
+import android.content.ContentResolver;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.view.DragEvent;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class GameView extends AppCompatActivity {
 
-    TextView txt1, target;
+    TextView txt1;
+    LinearLayout ln1, ln2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,22 +27,52 @@ public class GameView extends AppCompatActivity {
         setContentView(R.layout.activity_game_view);
 
         txt1 = findViewById(R.id.text1);
-        target = findViewById(R.id.target);
+        ln1 = findViewById(R.id.ln1);
+        ln2 = findViewById(R.id.ln2);
 
         txt1.setOnLongClickListener(longClickListener);
-        target.setOnLongClickListener(longClickListener);
+        ln1.setOnDragListener(dragListener);
+        ln2.setOnDragListener(dragListener);
     }
 
     View.OnLongClickListener longClickListener = new View.OnLongClickListener() {
         @Override
         public boolean onLongClick(View view) {
-
-            ClipData.Item item = new ClipData.Item((CharSequence) view.getTag());
+            ClipData.Item item = new ClipData.Item("");
             String[] mimeTypes = {ClipDescription.MIMETYPE_TEXT_PLAIN};
-            ClipData data = new ClipData(view.getTag().toString(), mimeTypes, item);
-            View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
+            ClipData data = new ClipData("", mimeTypes, item);
+            View.DragShadowBuilder dragShadowBuilder = new View.DragShadowBuilder(view);
+            view.startDragAndDrop(data, dragShadowBuilder, view, 0);
+            view.setVisibility(View.INVISIBLE);
+            return true;
+        }
+    };
 
-            view.startDragAndDrop(data, shadowBuilder, view, 0);
+    View.OnDragListener dragListener = new View.OnDragListener() {
+        @Override
+        public boolean onDrag(View view, DragEvent dragEvent) {
+            switch (dragEvent.getAction()) {
+                case DragEvent.ACTION_DRAG_STARTED:
+                    return dragEvent.getClipDescription().hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN);
+                case DragEvent.ACTION_DRAG_ENTERED:
+                case DragEvent.ACTION_DRAG_EXITED:
+                case DragEvent.ACTION_DRAG_ENDED:
+                    view.invalidate();
+                    return true;
+                case DragEvent.ACTION_DRAG_LOCATION:
+                    return true;
+                case DragEvent.ACTION_DROP:
+                    ClipData.Item item = dragEvent.getClipData().getItemAt(0);
+                    CharSequence dragData = item.getText();
+                    System.out.println(dragData);
+                    view.invalidate();
+                    View v = (View)(dragEvent.getLocalState());
+                    ViewGroup owner = (ViewGroup)(v.getParent());
+                    owner.removeView(v);
+                    ((LinearLayout)view).addView(v);
+                    v.setVisibility(View.VISIBLE);
+                    return true;
+            }
             return true;
         }
     };
