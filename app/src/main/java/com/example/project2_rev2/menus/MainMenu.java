b500312a9c2216875a6e149b54e3b18b8e6f251d
Guy Oz <109523947+GuyOz5252@users.noreply.github.com>
2022-10-31 00:49:55 +0200

    worked on main menu fragment layout

--- app/src/main/java/com/example/project2_rev2/menus/MainMenu.java
+++ app/src/main/java/com/example/project2_rev2/menus/MainMenu.java
@@ -6,24 +6,46 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageButton;
 
-import com.example.project2_rev2.MainMenuFragment;
 import com.example.project2_rev2.R;
 
-public class MainMenu extends AppCompatActivity {
+public class MainMenu extends AppCompatActivity implements View.OnTouchListener {
+
+    Fragment mainMenuFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        View decorView = getWindow().getDecorView();
+        int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        decorView.setSystemUiVisibility(flags);
+        decorView.setOnSystemUiVisibilityChangeListener(i -> decorView.setSystemUiVisibility(flags));
         setContentView(R.layout.activity_main_menu);
 
-        replaceFragment(new MainMenuFragment());
+        mainMenuFragment = new MainMenuFragment();
+
+        replaceFragment(mainMenuFragment);
     }
 
     public void replaceFragment(Fragment fragment) {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.frameLayout_mainMenu, fragment);
         fragmentTransaction.commit();
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        switch (view.getId()) {
+
+        }
+        return true;
     }
 }
--- app/src/main/java/com/example/project2_rev2/menus/MainMenuFragment.java
+++ app/src/main/java/com/example/project2_rev2/menus/MainMenuFragment.java
@@ -1,4 +1,4 @@
-package com.example.project2_rev2;
+package com.example.project2_rev2.menus;
 
 import android.os.Bundle;
 
@@ -8,12 +8,15 @@ import androidx.fragment.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+
+import com.example.project2_rev2.R;
 
 public class MainMenuFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main_menu, container, false);
-
     }
 }
